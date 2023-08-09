b25e0884498a211e97e8cfc90e2e5a21483c2d0e
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-08-09 04:47:54 +0100

    Start api server and complete logics to create and get all and one student

--- session4_servers2/apiServer.js
+++ session4_servers2/apiServer.js
@@ -1,25 +1,77 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
+const students = [];
+
+const bodyParser = (req, res, callback) => {
+	let requestBody = "";
+
+	try {
+		req.on("data", (chunk) => {
+			requestBody += chunk.toString();
+		});
+		req.on("end", () => {
+			if (requestBody.length) {
+				req.body = JSON.parse(requestBody);
+			}
+
+			callback(req, res);
+		});
+	} catch (error) {
+		console.log(error.message);
+		return error;
+	}
+};
+
+const handleRequest = (req, res) => {
 	if (req.url === "/") {
-		res.end("Welcome to our home page");
+		res.setHeader("Content-Type", "text/html");
+		res.writeHead(200);
+		res.write("Welcome to my API!");
+		return res.end();
 	}
 
 	if (req.url === "/v1/students" && req.method === "POST") {
-		// console.log(req.body);
-		let requestBody = "";
-
-		req.on("data", chunk => {
-            requestBody += chunk.toString();
-        });
-        req.on("end", () => {
-            console.log(requestBody);
-            console.log(JSON.parse(requestBody));
-            res.end("Data received");
-        });
-		// res.end("Data received");
+		const newStudent = {
+			id: Math.floor(Math.random() * 700),
+			...req.body,
+		};
+		students.push(newStudent);
+		res.setHeader("Content-Type", "application/json");
+		res.writeHead(201);
+		res.write(JSON.stringify({ message: "Student Added", data: req.body }));
+		return res.end();
 	}
-});
+
+	if (req.url === "/v1/students" && req.method === "GET") {
+		res.setHeader("Content-Type", "application/json");
+		res.writeHead(200);
+		if (students.length) {
+			res.write(JSON.stringify({ message: "Students Found", data: students }));
+		} else {
+			res.write(JSON.stringify({ message: "No Students", data: students }));
+		}
+		return res.end();
+	}
+
+	if (req.url.startsWith("/v1/students") && req.method === "GET") {
+		const id = Number(req.url.split('/')[3]);
+		const foundStudent = students.find(student => student.id === id);
+
+		res.setHeader("Content-Type", "application/json");
+		res.writeHead(200);
+		if (foundStudent) {
+			res.write(JSON.stringify({ message: "Student Found", data: foundStudent }));
+		} else {
+			res.write(JSON.stringify({ message: `Student with id ${id} not found`, data: null }));
+		}
+		return res.end();
+	}
+
+};
+
+const server = http.createServer((req, res) =>
+	bodyParser(req, res, handleRequest)
+);
 
 const PORT = 5002;
 server.listen(PORT, () => {
