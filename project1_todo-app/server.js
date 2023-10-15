7b48c82bdd6b87f8d7ea3507bca976f814b56915
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-10-15 23:51:27 +0100

    Develop the home page

--- project1_todo-app/server.js
+++ project1_todo-app/server.js
@@ -12,10 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.render("home");
 });
 
 app
