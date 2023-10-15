02b641361017327fcd7c26fa503794c95907b4ce
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-10-15 02:21:51 +0100

    Write request body validator middleware

---
+++ project1_todo-app/middlewares/validateRequest.js
@@ -0,0 +1,30 @@
+const joi = require("joi");
+
+exports.checkNewUser = async (req, res, next) => {
+	const schema = joi.object({
+		username: joi.string().required(),
+		email: joi.string().email().required(),
+		password: joi.string().required()
+	});
+
+	try {
+		const value = await schema.validateAsync(req.body);
+	} catch (error) {
+		return res.status(422).json({ message: error.details[0].message, data: null });
+	}
+	next();
+};
+
+exports.checkLogin = async (req, res, next) => {
+	const schema = joi.object({
+		email: joi.string().email().required(),
+		password: joi.string().required()
+	});
+
+	try {
+		const value = await schema.validateAsync(req.body);
+	} catch (error) {
+		return res.status(422).json({ message: error.details[0].message, data: null });
+	}
+	next();
+};
--- project1_todo-app/routes/authRoutes.js
+++ project1_todo-app/routes/authRoutes.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const authControllers = require("../controllers/authControllers");
+const {checkLogin, checkNewUser} = require("../middlewares/validateRequest")
 
 router
     .get("/signup", (req, res) => {
         res.send("GET Sign up")
     })
-    .post("/signup", authControllers.signup)
-    .post("/signin", authControllers.signin)
+    .post("/signup", checkNewUser, authControllers.signup)
+    .post("/signin", checkLogin, authControllers.signin)
     .get('/users', authControllers.getAllUsers)
 
 module.exports = router;
