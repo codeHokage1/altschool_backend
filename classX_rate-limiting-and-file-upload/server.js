dc8b72723f6ae2649c16314b6ac7e02790f50765
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-11-15 21:00:51 +0100

    Write logic to allow single file upload

--- classX_rate-limiting-and-file-upload/server.js
+++ classX_rate-limiting-and-file-upload/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const {rateLimit} = require('express-rate-limit');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+
 const app = express();
+
 
 const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 15 minutes
@@ -10,12 +14,21 @@ const limiter = rateLimit({
 	// store: ... , // Use an external store for consistency across multiple server instances.
 })
 
+
 // Apply the rate limiting middleware to all requests.
 app.use(limiter)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
+app.post('/files', upload.single('file'), function (req, res, next) {
+	return res.json({
+		success: true,
+		data: req.file
+	})
+  })
+  
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
