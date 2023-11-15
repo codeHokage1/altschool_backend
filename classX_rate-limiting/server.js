3be9ab26355dea80c9b9489b46f92427d2766019
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-11-15 20:06:26 +0100

    Write the logic for a basic rate limiter

---
+++ classX_rate-limiting/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {rateLimit} = require('express-rate-limit');
+const app = express();
+
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 15 minutes
+	limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Use an external store for consistency across multiple server instances.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
