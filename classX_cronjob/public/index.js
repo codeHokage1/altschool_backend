c78c7a2aaf6f412806ea4b4cada8ada71373db79
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-12-13 16:56:09 +0100

    Continue integration of FE and BE

--- classX_cronjob/public/index.js
+++ classX_cronjob/public/index.js
@@ -1,3 +1,51 @@
-const form = document.querySelector('#form');
-const actionSection = document.querySelector('.action-section');
+const form = document.querySelector("#form");
+const actionSection = document.querySelector(".action-section");
 
+const host = window.location.host;
+
+form.addEventListener("submit", async (e) => {
+	e.preventDefault();
+
+   // change the action section to a loading spinner
+   actionSection.innerHTML = `
+      <div class="d-flex justify-content-center">
+         <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+         </div>
+      </div>
+   `;
+
+   // send request to server
+	const data = {
+		username: form.username.value,
+		email: form.email.value,
+		date_of_birth: form.dob.value
+	};
+
+	try {
+		const response = await fetch(`http://${host}/birthdays`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(data)
+		});
+
+
+      // change the action section to a success message
+      if(response.status === 201) {
+         actionSection.innerHTML = `
+            <h3 class="text-center text-success">User created successfully</h3>
+         `;
+      }
+	} catch (error) {
+		console.log(error);
+
+      // change the action section to an error message
+      actionSection.innerHTML = `
+         <h3 class="text-center text-danger">An error occured</h3>
+      `;
+	}
+
+	console.log(data);
+});
